2e692b866ab18e1e3dc9e9c6dac0ae867aa6746b
Lídia Carvalho <lidiafcarvalho@outlook.com>
2025-06-18 08:38:31 -0300

    estrutura do repositório montada

---
+++ src/e2e/formularioLoginTest.js
@@ -0,0 +1,12 @@
+// Formulário de login
+// URL: https://the-internet.herokuapp.com/login
+
+// ○ Login: practice
+// ○ Senha: SuperSecretPassword!
+
+// Teste 1 - validar tratamento de erro campo username vazio 
+// Teste 2 - validar tratamento de erro campo senha vazio 
+// Teste 3 - validar cenário de login com sucesso
+// Teste 4 - validar tratamento de erro credenciais incorretas 
+// teste 5 - validar tratamento de erro acesso sem autenticação
+
---
+++ src/e2e/uploadArquivosTest.js
@@ -0,0 +1,9 @@
+// Upload de Arquivos
+// URL: https://the-internet.herokuapp.com/upload
+
+// Esta página contém um componente de upload que permite envio de arquivos via botão de
+// seleção ou área de drag and drop.
+
+// Teste 1 - validar upload por meio do botao "choose file"
+// Teste 2 - validar upload por meio da área de upload marcada
+// Teste 3 - validar upload de uma sequência de arquivos
